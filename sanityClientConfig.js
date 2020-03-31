acde9b8f20cb491ca078865763d5b69acd2b0ac1
Stian Conradsen <stian.conradsen@sparebank1.no>
2020-03-31 11:20:47 +0200

    SpareBank 1 Kahoot

--- sanityClientConfig.js
+++ sanityClientConfig.js
@@ -1,6 +1,6 @@
 export const sanityClientConfig = {
   // replace this id with the one you find in /studio/sanity.json
-  projectId: "puj7p168",
+  projectId: "qgmwd7vx",
   // make sure that the dataset name is the same as the one in /studio/sanity.json
   dataset: "production",
   useCdn: false
